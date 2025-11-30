4c25fe7a84fe0ce17964fa8c3b98dd94e7e75db5
voelkerh <henriette.voelker@posteo.de>
2025-11-30 14:03:08 +0100

    Add load and save methods to RecentCapturesStore.java

--- app/src/main/java/com/benskitchen/capturingthepast/persistence/RecentCapturesStore.java
+++ app/src/main/java/com/benskitchen/capturingthepast/persistence/RecentCapturesStore.java
@@ -1,4 +1,8 @@
 package com.benskitchen.capturingthepast.persistence;
 
+import java.util.List;
+
 public interface RecentCapturesStore {
+    List<String> loadRecentFiles();
+    boolean saveRecentFiles(List<String> files);
 }
