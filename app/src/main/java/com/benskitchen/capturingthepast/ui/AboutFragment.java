dba64007890a33d0fc11ee121bed3d22892ae8ac
voelkerh <henriette.voelker@posteo.de>
2025-12-19 17:04:42 +0100

    Create content for AboutFragment

--- app/src/main/java/com/benskitchen/capturingthepast/ui/AboutFragment.java
+++ app/src/main/java/com/benskitchen/capturingthepast/ui/AboutFragment.java
@@ -1,6 +1,8 @@
 package com.benskitchen.capturingthepast.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +14,15 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView aboutText = view.findViewById(R.id.about_text);
+        aboutText.setText(HtmlCompat.fromHtml(
+                getString(R.string.about_fragment_text),
+                HtmlCompat.FROM_HTML_MODE_COMPACT
+        ));
+
+        return view;
     }
 }
