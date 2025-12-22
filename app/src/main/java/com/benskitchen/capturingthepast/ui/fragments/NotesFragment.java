67c0b9aebe963c0e00b681085ef441c53ebce9e6
voelkerh <henriette.voelker@posteo.de>
2025-12-22 15:57:31 +0100

    Implement basic ui content display in notes fragment

--- app/src/main/java/com/benskitchen/capturingthepast/ui/fragments/NotesFragment.java
+++ app/src/main/java/com/benskitchen/capturingthepast/ui/fragments/NotesFragment.java
@@ -1,18 +1,59 @@
 package com.benskitchen.capturingthepast.ui.fragments;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import capturingthepast.R;
+import com.benskitchen.capturingthepast.domainLogic.Capture;
+import com.benskitchen.capturingthepast.domainLogic.RecentCapturesRepository;
+
+import java.util.List;
 
 public class NotesFragment extends Fragment {
+
+    RecentCapturesRepository recentCapturesRepository;
+
+    public NotesFragment(RecentCapturesRepository recentCapturesRepository) {
+        this.recentCapturesRepository = recentCapturesRepository;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        TextView notesInfo = view.findViewById(R.id.notes_text_info);
+        notesInfo.setText(HtmlCompat.fromHtml(
+                getString(R.string.notes_fragment_text_info),
+                HtmlCompat.FROM_HTML_MODE_COMPACT
+        ));
+
+        TextView notesText = view.findViewById(R.id.notes_text_files);
+        notesText.setText(HtmlCompat.fromHtml(
+                getString(R.string.notes_fragment_text_files),
+                HtmlCompat.FROM_HTML_MODE_COMPACT
+        ));
+
+        TextView notesList = view.findViewById(R.id.notes_capture_list);
+        notesList.setText(getFileList());
+
+        return view;
+    }
+
+    private String getFileList() {
+        List<Capture> recentFiles = recentCapturesRepository.getRecentCaptures();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = recentFiles.size() - 1; i >= 0; i--) {
+            Capture capture = recentFiles.get(i);
+            sb.append("File: ").append(capture.getFileName()).append("\n");
+            sb.append("Note: ").append(capture.getNote()).append("\n\n");
+        }
+
+        return sb.toString();
     }
 }
