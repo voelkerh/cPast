6d8229fc69147de45dab9bdcca332308e7f7550a
voelkerh <henriette.voelker@posteo.de>
2025-12-22 15:54:24 +0100

    Use Capture in RecentCapturesRepository and -store and implement JSON parsers for capture object

--- app/src/main/java/com/benskitchen/capturingthepast/persistence/RecentCapturesStore.java
+++ app/src/main/java/com/benskitchen/capturingthepast/persistence/RecentCapturesStore.java
@@ -1,8 +1,10 @@
 package com.benskitchen.capturingthepast.persistence;
 
+import com.benskitchen.capturingthepast.domainLogic.Capture;
+
 import java.util.List;
 
 public interface RecentCapturesStore {
-    List<String> loadRecentFiles();
-    boolean saveRecentFiles(List<String> files);
+    List<Capture> loadRecentFiles();
+    boolean saveRecentFiles(List<Capture> files);
 }
