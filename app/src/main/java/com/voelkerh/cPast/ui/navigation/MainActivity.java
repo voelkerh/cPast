9c8e7a6568b4060f86346a2b1393e2bc2f8bfcc2
voelkerh <henriette.voelker@posteo.de>
2026-01-03 17:47:53 +0100

    Remove deprecated method onBackPressed and avoid using resource ids in switch statements

--- app/src/main/java/com/voelkerh/cPast/ui/navigation/MainActivity.java
+++ app/src/main/java/com/voelkerh/cPast/ui/navigation/MainActivity.java
@@ -5,12 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import com.google.android.material.navigation.NavigationView;
 import com.voelkerh.cPast.R;
 import com.voelkerh.cPast.ui.about.AboutFragment;
@@ -39,6 +41,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (!isHomeFragmentVisible()) {
+                    loadFragment(new HomeFragment());
+                    navigationView.setCheckedItem(R.id.nav_home);
+                } else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
+        });
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
@@ -47,39 +64,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                break;
-            case R.id.nav_photos:
-                openGallery();
-                break;
-            case R.id.nav_notes:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NotesFragment()).commit();
-                break;
-            case R.id.nav_help:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HelpFragment()).commit();
-                break;
-            case R.id.nav_about:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
-                break;
-            case R.id.nav_exit:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                break;
+        int itemId = menuItem.getItemId();
 
+        if (itemId == R.id.nav_home || itemId == R.id.nav_exit) {
+            loadFragment(new HomeFragment());
+        } else if (itemId == R.id.nav_photos) {
+            openGallery();
+        } else if (itemId == R.id.nav_notes) {
+            loadFragment(new NotesFragment());
+        } else if (itemId == R.id.nav_help) {
+            loadFragment(new HelpFragment());
+        } else if (itemId == R.id.nav_about) {
+            loadFragment(new AboutFragment());
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+    }
+
+    private boolean isHomeFragmentVisible() {
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        return currentFragment instanceof HomeFragment;
     }
 
     private void openGallery() {
